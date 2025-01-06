4334d36ceeeb71342da94b4e620248fb8253595f
James Smith <mrsmith@aliyun.com>
2025-01-07 07:46:38 +0800

    clean up uneccessary await/async

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,8 +2,8 @@ import { User } from '../models/userModel.js'
 import { setAlert } from '../utils/alert.js'
 import { redirect } from '../utils/redirect.js'
 
-export const getUserOrThrow = async userId => {
-  const { data: user, errors } = await User.findById(userId)
+export const getUserOrThrow = userId => {
+  const { data: user, errors } = User.findById(userId)
   if (errors) {
     const error = new Error(errors.all)
     error.status = 404
@@ -12,38 +12,38 @@ export const getUserOrThrow = async userId => {
   return user
 }
 
-export const allUsers = async (context, _request) => {
-  const { data: users, errors } = await User.getAll()
+export const allUsers = (context, _request) => {
+  const { data: users, errors } = User.getAll()
   return context.sendPage('user/index.html', {
     users,
     errors,
   })
 }
 
-export const showUser = async (context, _request, params) => {
-  const user = await getUserOrThrow(params.id)
+export const showUser = (context, _request, params) => {
+  const user = getUserOrThrow(params.id)
   return context.sendPage('user/detail.html', { selectedUser: user })
 }
 
-export const showUserEditForm = async (context, _request, params) => {
-  const user = await getUserOrThrow(params.id)
+export const showUserEditForm = (context, _request, params) => {
+  const user = getUserOrThrow(params.id)
   return context.sendPage('user/edit.html', { selectedUser: user })
 }
 
-export const showChangePasswordForm = async (context, _request, params) => {
-  const user = await getUserOrThrow(params.id)
+export const showChangePasswordForm = (context, _request, params) => {
+  const user = getUserOrThrow(params.id)
   return context.sendPage('user/change-password.html', { selectedUser: user })
 }
 
-export const editUser = async (context, _request, params) => {
+export const editUser = (context, _request, params) => {
   if (context.user?.role !== 'admin' && context.body?.role) {
     return context.sendPage('403.html', {
       error: new Error('You do not have permission to edit this user.'),
     })
   }
-  const { errors } = await User.update(params.id, context.body)
+  const { errors } = User.update(params.id, context.body)
   if (errors) {
-    const user = await getUserOrThrow(params.id)
+    const user = getUserOrThrow(params.id)
     return context.sendPage('user/edit.html', { selectedUser: user, errors })
   }
   setAlert(
@@ -54,18 +54,18 @@ export const editUser = async (context, _request, params) => {
   return redirect(context, `/user/${params.id}`)
 }
 
-export const deleteUser = async (context, _request, params) => {
-  const { errors } = await User.remove(params.id)
+export const deleteUser = (context, _request, params) => {
+  const { errors } = User.remove(params.id)
   if (errors) {
-    const user = await getUserOrThrow(params.id)
+    const user = getUserOrThrow(params.id)
     return context.sendPage('user/detail', { errors, selectedUser: user })
   }
   setAlert(context, 'User deleted successfully.', 'success')
   return redirect(context, '/user')
 }
 
-export const createUser = async (context, _request) => {
-  const { data, errors } = await User.create(context.body)
+export const createUser = (context, _request) => {
+  const { data, errors } = User.create(context.body)
   if (errors) {
     return context.sendPage('user/create.html', {
       newUser: context.body,
