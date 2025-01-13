36eb372dd24c748992f1d098a442e021bbc623f0
James Smith <mrsmith@aliyun.com>
2025-01-13 09:44:45 +0800

    clear console on server startup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import { createServer } from './src/app.js'
 
 const port = process.env.PORT || 3000
 const hostname = process.env.HOSTNAME || 'localhost'
+
+console.clear()
 createServer(port, hostname)
 
 console.info(`🚀 Server is running on http://${hostname}:${port}`)
