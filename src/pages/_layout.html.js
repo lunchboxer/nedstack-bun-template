005cf77a37ecc4fd1cb0be5c2fe1dc78ae27b73f
James Smith <mrsmith@aliyun.com>
2025-09-05 20:44:04 +0800

    minor formatting

--- src/pages/_layout.html.js
+++ src/pages/_layout.html.js
@@ -33,8 +33,9 @@ export const layout = ({ title, headExtras, content, data }) => html`
 
   ${footer}
   <script src="/public/scripts.js" type="text/javascript"></script>
-  ${process.env.NODE_ENV === 'development' &&
-  html`
+  ${
+    process.env.NODE_ENV === 'development' &&
+    html`
   <script src="/public/hot-reloader.js" type="text/javascript" defer></script>`
   }
 </body>
