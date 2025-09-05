6037b03ce5e97780d511bd586cf3a62efaa7d818
James Smith <mrsmith@aliyun.com>
2025-09-05 20:42:54 +0800

    allow font cacheing in dev. to avoid flash on every page load

--- src/pages/_layout.html.js
+++ src/pages/_layout.html.js
@@ -15,6 +15,8 @@ export const layout = ({ title, headExtras, content, data }) => html`
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="view-transition" content="auto">
 
+  <link rel="preload" href="/public/fonts/JetBrainsMono-Regular.woff2" as="font" type="font/woff2" crossorigin>
+  <link rel="preload" href="/public/fonts/JetBrainsMono-ExtraBold.woff2" as="font" type="font/woff2" crossorigin>
   <link rel="stylesheet" href="/public/styles.css" type="text/css" />
   ${headExtras}
 </head>
@@ -31,9 +33,8 @@ export const layout = ({ title, headExtras, content, data }) => html`
 
   ${footer}
   <script src="/public/scripts.js" type="text/javascript"></script>
-  ${
-    process.env.NODE_ENV === 'development' &&
-    html`
+  ${process.env.NODE_ENV === 'development' &&
+  html`
   <script src="/public/hot-reloader.js" type="text/javascript" defer></script>`
   }
 </body>
