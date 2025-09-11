1b465f7b275708a86a0bf2f80ba16e7924132927
James Smith <mrsmith@aliyun.com>
2025-09-11 10:56:14 +0800

    add clear error code on port in use, fix hot reloading

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ import { createServer } from './src/app.js'
 const port = process.env.PORT || 3000
 const hostname = process.env.HOSTNAME || 'localhost'
 
-createServer(port, hostname)
+try {
+  createServer(port, hostname)
+} catch (error) {
+  if (error.code === 'EADDRINUSE') {
+    console.error(`🚨 Port ${port} is already in use`)
+    process.exit(1)
+  }
+  throw error
+}
 
 console.info(`🚀 Server is running on http://${hostname}:${port}`)
